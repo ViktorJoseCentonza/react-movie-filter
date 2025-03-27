aca6260bcccd3e50f1a1d1468d1e6bed32f046f5
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-03-27 14:38:29 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import film_data from "./data/films";
+import "./App.css";
+
+const App = () => {
+  const [data, setData] = useState('Please choose one option');
+  console.log(film_data.filter((object) => object.genre != data))
+  const options = [
+    "Fantascienza",
+    "Thriller",
+    "Romantico",
+    "Azione",
+  ];
+  const onOptionChangeHandler = (event) => {
+    setData(event.target.value);
+    console.log(
+      "User Selected Value - ",
+      event.target.value
+    );
+
+    console.log(film_data)
+  };
+  return (
+    <>
+
+      <select onChange={onOptionChangeHandler}>
+        <option>Please choose one option</option>
+        {options.map((option, index) => {
+          return (
+            <option key={index}>
+              {option}
+            </option>
+          );
+        })}
+      </select>
+      <h3>You selected: {data} </h3>
+
+      {data != 'Please choose one option' ? film_data.filter((object) => object.genre === data).map((object, index) => (
+        <h2 key={`film-${index}`}>{object.title}</h2>)) : film_data.map((object, index) => (
+          <h2 key={`film-${index}`}>{object.title}</h2>
+        ))}
+    </>
+
+
+
+
+
+  );
+};
+
+export default App;
